3d7bb192c3c4ef1908176b89e9216f3aab9127dd
Arnaud DOLE <dole.arnaud@gmail.com>
2022-10-06 17:20:07 +0200

    use temp rather than root

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 sudo sysctl -w vm.max_map_count=262144
 docker compose down -v
-sudo cp -f ./filebeat/config/filebeat.yml /root/
-sudo cp -f ./logstash/pipeline/logstash.conf /root/
+sudo cp -f ./filebeat/config/filebeat.yml /tmp/
+sudo cp -f ./logstash/pipeline/logstash.conf /tmp/
 docker compose up -d --build
 
